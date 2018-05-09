ee5dd3f9d01d22113dcdfce445d625e1082b748c
Thom Bilton <thombilton@gmail.com>
2018-05-09 16:09:08 +1200

    Made it work v1

--- index.js
+++ index.js
@@ -20,38 +20,6 @@ server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and listening on port" + process.env.PORT);
 });
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://ping:ping@ds117070.mlab.com:17070/chatbot"
-
-var result;
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("chatbot");
-    dbo.collection("papers").find({_id:'COMP500'}, {_id:0, paperName:1, year:0}).toArray(function(err, result2) {
-        if (err) throw err;
-        result = result2;
-        //console.log(result[0]._id);
-        db.close();
-    });
-});
-
-
-
-
-
-
-/*MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    var query = { _id: "COMP500" };
-    dbo.collection("papers").find(query).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-    });
-});*/
-
 
 
 
