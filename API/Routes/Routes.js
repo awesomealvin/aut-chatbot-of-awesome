93c791d49c17a2aab6d14e7f2e52071e2c79b88e
awesomealvin <fookinhellvin@outlook.com>
2018-05-28 08:55:02 +1200

    comments

--- API/Routes/Routes.js
+++ API/Routes/Routes.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * @author Thom Bilton
+ * The controller class for sending requests around the server.
+ * When a JSON request is sent to the server address it is routed to /Controllers/dataController and proccessed there.
+ * If other routes were needed for other reuqests they would be defined here.
+ **/
+
+
 var express = require('express');
 module.exports = function(app) {
   var dataController = require('../Controllers/dataController');
@@ -11,5 +19,3 @@ app.get('/',function(req,res){
   app.route('/')
     .post(dataController.processRequest);
 };
-
-
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/**
+ * Declorations for server functions
+ */
+
 var express  = require('express'),
 bodyParser   = require('body-parser'),
 http         = require('http'),
@@ -7,12 +11,17 @@ config       = require('./config'),
 server       = express(),
 mongoose     = require('mongoose'),
 paperInfo     = require('./API/Models/paperInfo');
-//created model loading here
-// mongoose instance connection url connection
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.dbUrl);
 
+/**
+ * File called when server stars.
+ * registers routes with the server and listens for requests at port:8000.
+ * Enables the acceptance of JSON files as requests to the server.
+ * @author Thom Bilton
+ */
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
@@ -21,7 +30,3 @@ routes(server); //register the route
 server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and listening on port" + process.env.PORT);
 });
-
-
-
-
